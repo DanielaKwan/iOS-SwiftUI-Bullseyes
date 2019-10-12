22df521e6d0d09f5a0b781834999ca667101b5fd
Daniela Kwan <daniela.camila.kwan@gmail.com>
2019-10-12 01:05:23 -0500

    [First part]

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -10,37 +10,43 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State var alertIsVisible: Bool = false 
+    @State var alertIsVisible: Bool = false
     
     var body: some View {
-        VStack {
-            Text("Welcome to my first app!")
-                .fontWeight(.semibold)
-                .foregroundColor(Color.orange)
-                .multilineTextAlignment(.center)
-            Button(action:{
-                print("Button-action - self.alertIsVisible = \(self.alertIsVisible)")
-                let nombre:String = "Hit you!"
-                print(nombre, separator: " - ", terminator: "\n")
-                self.alertIsVisible = true
-                print("Button-action - self.alertIsVisible = \(self.alertIsVisible)")
-            }) {
-                Text("Hit you!")
-                    .foregroundColor(Color.purple)
-            }.alert(isPresented: $alertIsVisible) { () -> Alert in
-                print("Button-alert - selfalertIsVisible = \(self.alertIsVisible)")
+            VStack {
+                // Target row
+                HStack{
+                Text("Put the bullseye as lose as you can to:")
+                }
+                // Slider row
                 
-                return Alert(title: Text("Hello there!"),
-                             message: Text("This is my first pop-up."),
-                             dismissButton: .default(Text("Awesome!")))
+                // Button row
+                
+                Button(action:{
+                    print("Button-action - self.alertIsVisible = \(self.alertIsVisible)")
+                    let nombre:String = "Hit Me!"
+                    print(nombre, separator: " - ", terminator: "\n")
+                    self.alertIsVisible = true
+                    print("Button-action - self.alertIsVisible = \(self.alertIsVisible)")
+                }) {
+                    Text("Hit Me!")
+                        .foregroundColor(Color.purple)
+                }.alert(isPresented: $alertIsVisible) { () -> Alert in
+                    print("Button-alert - selfalertIsVisible = \(self.alertIsVisible)")
+                    
+                    return Alert(title: Text("Hello there!"),
+                                 message: Text("This is my first pop-up."),
+                                 dismissButton: .default(Text("Awesome!")))
+                }
             }
         }
     }
-}
 
-struct ContentView_Previews: PreviewProvider {
+struct ContentView_Previews:
+    PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView() .previewLayout(
+            .fixed(width: 896, height: 414))
     }
 }
 
